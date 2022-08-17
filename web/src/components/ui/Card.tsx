173b4f8947c6d715f5fd17c06bc6203dd9d1764a
i7p7x0 <i7p7x0@gmail.com>
2022-08-17 11:15:49 +0545

    added skeleton card

---
+++ web/src/components/ui/Card.tsx
@@ -0,0 +1,47 @@
+import { FC } from "react";
+import classes from "./card.module.css";
+import { AiFillLike, AiFillDislike } from "react-icons/ai";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+import { MdReportProblem } from "react-icons/md";
+import Button from "react-bootstrap/Button";
+
+const Card: FC = () => {
+  return (
+    <div className={classes["card-container"]}>
+      <div className={classes["card-head"]}>
+        <h4>authorname</h4>
+        <hr />
+      </div>
+
+      <div className={classes["card-body"]}>
+        <FaQuoteLeft />
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto provident maiores consectetur laboriosam
+          eaque, aspernatur, aut totam doloremque distinctio quaerat repudiandae! Libero fuga laudantium omnis. Ipsa
+          temporibus expedita nesciunt maxime deserunt molestias repudiandae tempore libero! Dicta soluta, sapiente
+          deleniti quasi nihil, animi ullam officiis, libero est fuga ipsam saepe optio!
+        </p>
+        <FaQuoteRight />
+        <hr />
+      </div>
+
+      <div className={classes["card-tail"]}>
+        <div className={classes["button-container"]}>
+          <Button variant="success" className="mx-1">
+            like
+          </Button>
+          <Button variant="danger" className="mx-1">
+            dislike
+          </Button>
+        </div>
+        <div className={classes["report-container"]}>
+          <Button variant="secondary">
+            <MdReportProblem size={24} color="#FFB200" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- web/src/pages/HomePage.tsx
+++ web/src/pages/HomePage.tsx
@@ -1,8 +1,13 @@
 import { FC } from "react";
 import classes from "./home-page.module.css";
+import Card from "../components/ui/Card";
 
 const HomePage: FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Card />
+    </div>
+  );
 };
 
 export default HomePage;
